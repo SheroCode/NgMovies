0a718ce111c1aa3ff554c413a9e6f0bdd8a8ae18
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-07-02 03:53:55 +0300

    add languages selections

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -2,16 +2,33 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WishlistService } from '../../services/wishlist-service';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { MovieService } from '../../services/movie-service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule,NgbCollapseModule],
+  imports: [RouterModule, NgbCollapseModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss'
+  styleUrl: './navbar.scss',
 })
 export class Navbar {
-wishlistService=inject(WishlistService);
-counter=this.wishlistService.counter;
-	isMenuCollapsed = true;
+  wishlistService = inject(WishlistService);
+  movieService = inject(MovieService);
 
+  counter = this.wishlistService.counter;
+  isMenuCollapsed = true;
+  languages = [
+    { code: 'en', label: 'English' },
+    { code: 'ar', label: 'Arabic' },
+    { code: 'fr', label: 'French' },
+    { code: 'zh', label: 'Chinese' },
+  ];
+
+  selectedLanguage = this.movieService.language();
+
+  onLanguageChange(event: Event) {
+    const lang = (event.target as HTMLSelectElement).value;
+    this.selectedLanguage = lang;
+    this.movieService.setLanguage(lang);
+    document.body.dir = lang === 'ar' ? 'rtl' : 'ltr';
+  }
 }
--- src/app/services/movie-service.ts
+++ src/app/services/movie-service.ts
@@ -1,7 +1,8 @@
+// movie-service.ts
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { map } from 'rxjs';
-import { MovieResults , NowPlayingResponse } from '../interfaces/interfaces';
+import { MovieResults, NowPlayingResponse } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -9,31 +10,36 @@ import { MovieResults , NowPlayingResponse } from '../interfaces/interfaces';
 export class MovieService {
   private http = inject(HttpClient);
   private readonly BASE_URL = 'https://api.themoviedb.org/3';
-  private readonly TOKEN =
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzYxMjA3Yzk4MDI1ZjBkZDlkN2RjMTQ3NjJiNzcyMyIsIm5iZiI6MTc0NzM2NDkyNC4zMzcsInN1YiI6IjY4MjZhYzNjOTA1OTk2NTJhZWFkYTc5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sen0exBSPtLuOdBL55OGwo-8LcSp4CkWERe8SVa7nP4';
+  private readonly TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzYxMjA3Yzk4MDI1ZjBkZDlkN2RjMTQ3NjJiNzcyMyIsIm5iZiI6MTc0NzM2NDkyNC4zMzcsInN1YiI6IjY4MjZhYzNjOTA1OTk2NTJhZWFkYTc5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sen0exBSPtLuOdBL55OGwo-8LcSp4CkWERe8SVa7nP4';
   private headers = new HttpHeaders({
     accept: 'application/json',
-    Authorization: `Bearer ${this.TOKEN} `,
+    Authorization: `Bearer ${this.TOKEN}`,
   });
-  // / signal to hold movies
+
   nowPlaying = signal<MovieResults[]>([]);
   isLoading = signal(false);
   currentPage = signal<number>(1);
   error = signal<string | null>(null);
+  language = signal('en');
 
   setCurrentPage(page: number) {
     this.currentPage.set(page);
   }
-  
+
+  setLanguage(lang: string) {
+    this.language.set(lang);
+    this.loadNowPlaying();
+  }
+
   loadNowPlaying() {
     this.isLoading.set(true);
     this.error.set(null);
+    const lang = this.language();
+
     this.http
       .get<NowPlayingResponse>(
-        `${this.BASE_URL}/movie/now_playing?language=en-US&page=${this.currentPage()}`,
-        {
-          headers: this.headers,
-        }
+        `${this.BASE_URL}/movie/now_playing?language=${lang}&page=${this.currentPage()}`,
+        { headers: this.headers }
       )
       .pipe(map((res) => res.results))
       .subscribe({
@@ -41,7 +47,7 @@ export class MovieService {
           this.nowPlaying.set(movies);
           this.isLoading.set(false);
         },
-        error: (err) => {
+        error: () => {
           this.error.set('Failed to load movies');
           this.isLoading.set(false);
         },
